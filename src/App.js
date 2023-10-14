78ea94cff1dc2c1dc509fb8cd54541835597da7d
Mixaqi <mixaqimixaqi@mail.ru>
2023-10-14 20:52:17 +0300

    added simple layout without structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='app-wrapper'>
+      <header className='header'>
+        <img src='https://cdn.logo.com/hotlink-ok/logo-social.png'></img>
       </header>
-    </div>
-  );
+      <nav className='nav'>
+        <div>
+          <a>Profile</a>
+        </div>
+        <div>
+          <a>Messages</a>
+        </div>
+        <div>
+          <a>News</a>
+        </div>
+        <div>
+          <a>Music</a>
+        </div>
+        <div>
+          <a>Settings</a>
+        </div>
+      </nav>
+      <div className='content'>
+      <div>
+        <img src ='https://www.industrialempathy.com/img/remote/ZiClJf-1920w.jpg' />
+      </div>
+      <div>
+        ava + description
+      </div>
+      <div>
+        My posts
+      </div>
+      <div>
+        New post
+      </div>
+      <div>
+        <div>
+          post1
+        </div>
+        <div>
+          post 2
+        </div>
+      </div>
+      </div>
+    </div>);
 }
 
 export default App;
